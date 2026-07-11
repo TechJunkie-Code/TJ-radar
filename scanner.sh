b8ddc4ffeb387e7d7fd397e904c85c0c23cd6030
TJ <295429486+TechJunkie-Code@users.noreply.github.com>
2026-07-11 16:37:23 +0330

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -6,7 +6,8 @@ echo -e "\e[1;36m=========================================\e[0m"
 echo ""
 echo -e "\e[1;33m[*] Fetching latest configs...\e[0m"
 
-curl -s https://raw.githubusercontent.com/yebekhe/V2RayConfig/main/v2ray/vless.txt > all_configs.txt
+LINK="https://raw.githubusercontent.com/yebekhe/V2RayConfig/main/v2ray/vless.txt"
+curl -s $LINK > all_configs.txt
 
 if [ -s all_configs.txt ]; then
     echo -e "\e[1;32m[+] Downloaded Successfully!\e[0m"
@@ -14,7 +15,7 @@ if [ -s all_configs.txt ]; then
     echo -e "\e[1;36m--- TOP 5 VLESS CONFIGS --- \e[0m"
     head -n 5 all_configs.txt
 else
-    echo -e "\e[1;31m[!] Error: Check your internet connection.\e[0m"
+    echo -e "\e[1;31m[!] Error: Check your connection or source.\e[0m"
 fi
 
 echo ""
